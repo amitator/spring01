e34feb79c787b6edef3f69e76f4ac8498c45393f
Igor Prus <prus.igor@gmail.com>
2018-12-19 08:22:07 +0300

    l18n not working

--- src/main/java/ru/otus/spring/hw01/locale/Locale.java
+++ src/main/java/ru/otus/spring/hw01/locale/Locale.java
@@ -2,14 +2,18 @@ package ru.otus.spring.hw01.locale;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Service;
 
-public class Local {
+@Service
+@Configuration
+public class Locale {
 
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
-        ms.setBasename("i18n/bundle");
+        ms.setBasename("classpath:i18n/bundle");
         ms.setDefaultEncoding("UTF-8");
         return ms;
     }
--- src/main/java/ru/otus/spring/hw01/services/UserNameImpl.java
+++ src/main/java/ru/otus/spring/hw01/services/UserNameImpl.java
@@ -1,7 +1,10 @@
 package ru.otus.spring.hw01.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -14,10 +17,13 @@ public class UserNameImpl implements UserName {
      *
      * @return User name
      */
+
+    @Autowired
+    private MessageSource messageSource;
+
     public String getUserName() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter your name: ");
-
+        System.out.print(messageSource.getMessage("welcome.msg", new String[]{"user"}, Locale.ENGLISH));
         return scanner.nextLine();
     }
 }
