6702b4e37e60c318a9cdfabe54b18349489d93b4
Igor Prus <prus.igor@gmail.com>
2018-12-25 17:08:17 +0300

    fixed @Autowiring in QuestionsReaderImpl and fixed constructor

--- src/main/java/ru/otus/spring/hw01/services/QuestionsReaderImpl.java
+++ src/main/java/ru/otus/spring/hw01/services/QuestionsReaderImpl.java
@@ -2,7 +2,6 @@ package ru.otus.spring.hw01.services;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
@@ -19,12 +18,12 @@ import java.util.Locale;
 @Service
 public class QuestionsReaderImpl implements QuestionsReader {
 
-    @Autowired
     private MessageSource messageSource;
 
     private String fileName;
 
     public QuestionsReaderImpl(MessageSource messageSource){
+        this.messageSource = messageSource;
         Locale locale = LocaleContextHolder.getLocale();
         fileName = messageSource.getMessage("file.name", new String[]{""}, locale);
     }
