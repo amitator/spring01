2550973cee2058d30f57f681faada21bbf1a58e7
Igor Prus <prus.igor@gmail.com>
2018-12-25 17:29:41 +0300

    fixed annotation fixed ApplicationContext

--- src/main/java/ru/otus/spring/hw01/Main.java
+++ src/main/java/ru/otus/spring/hw01/Main.java
@@ -1,38 +1,18 @@
 package ru.otus.spring.hw01;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.otus.spring.hw01.services.Quiz;
-import ru.otus.spring.hw01.services.UserName;
+import org.springframework.context.ApplicationContext;
+import ru.otus.spring.hw01.services.QuizImpl;
 
-@RestController
-@ComponentScan
-@Configuration
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Main {
-
-    @Autowired
-    private Quiz quiz;
-
-    @Autowired
-    private UserName userName;
 
     public static void main(String[] args) {
 
-        SpringApplication.run(Main.class, args);
+        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+        ctx.getBean(QuizImpl.class).run();
 
     }
 
-    @RequestMapping("/")
-    String home() {
-        quiz.run();
-        return "Hello World!";
-    }
 }
