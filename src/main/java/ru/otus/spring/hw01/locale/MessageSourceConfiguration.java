a97e708963c73d662c331882e157fd3d5cc17f41
Igor Prus <prus.igor@gmail.com>
2018-12-28 13:47:24 +0300

    renamed Locale to MessageSourceConfiguration

--- src/main/java/ru/otus/spring/hw01/locale/MessageSourceConfiguration.java
+++ src/main/java/ru/otus/spring/hw01/locale/MessageSourceConfiguration.java
@@ -4,11 +4,9 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.stereotype.Service;
 
-@Service
 @Configuration
-public class Locale {
+public class MessageSourceConfiguration {
 
     @Bean
     public MessageSource messageSource() {
